0a4e6c6bf5993bf8512632620d42b60d8083e924
oliwka777goat <oliwiabartosiak2009@gmail.com>
2025-10-12 16:06:25 +0200

    Flight.java

--- src/Flight.java
+++ src/Flight.java
@@ -1,3 +1,24 @@
 //klasa nadrzędna
 public class Flight {
+    protected  String flightNumber;
+    protected String destination;
+    protected String date;
+
+    public Flight(String flightNumber, String destination, String date) {
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.date = date;
+    }
+
+    // enkapsulacja - prywatne pole/gettery/settery, nie mozna nadpisac obiektu
+    // Getter (metoda pobierająca) zwraca wartość pola,
+    //setter (metoda ustawiająca) pozwala na jej zmianę
+
+
+    public String getFlightNumber() { return flightNumber; }
+    public void setFlightNumber(String flightNumber) { this.flightNumber = flightNumber;} //setter
+    public String getDestination() {return  destination;} //getter
+    public void setDestination(String destination) { this.destination = destination; }
+    public String getDate() { return  date; }
+    public void setDate(String date) { this.date = date; }
 }
