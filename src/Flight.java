dd972682cb327959abc613b8d1642f2e3b6415e0
oliwka777goat <oliwiabartosiak2009@gmail.com>
2025-10-16 10:44:26 +0200

    Refactor Flight class with static fields and method
    Refactor Flight class to use static fields and add CreateFlight method.

--- src/Flight.java
+++ src/Flight.java
@@ -22,3 +22,45 @@ public class Flight {
     public String getDate() { return  date; }
     public void setDate(String date) { this.date = date; }
 }
+
+import java.util.Scanner;
+
+//klasa nadrzędna, klasa stw lotow
+
+public class Flight {
+    public static String flightNumber;
+    public static String flightdestination;
+    public static String flightdate;
+
+    Scanner scanner = new Scanner(System.in);
+
+//    public Flight(String flightNumber, String destination, String date) {
+//
+//        this.flightNumber = flightNumber;
+//        this.destination = destination;
+//        this.date = date;
+//    }
+//
+    public void CreateFlight(){
+
+                System.out.println("Numer lotu: " + getFlightNumber());
+                System.out.println("Cel: " + getDestination());
+                System.out.println(("Data: " + getDate()));
+
+
+    }
+    // enkapsulacja - prywatne pole/gettery/settery, nie mozna nadpisac obiektu
+    // Getter (metoda pobierająca) zwraca wartość pola,
+    //setter (metoda ustawiająca) pozwala na jej zmianę
+
+
+
+    public String getFlightNumber() { return flightNumber; }
+//    public void setFlightNumber(String flightNumber) { this.flightNumber = flightNumber;} //setter
+    public String getDestination() {return  flightdestination;} //getter
+    public void setDestination(String destination) { this.flightdestination = destination; }
+    public String getDate() { return  flightdate; }
+    public void setDate(String date) { this.flightdate = date; }
+
+
+}
